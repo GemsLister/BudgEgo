47f0e8cd400a3f27bcedf3061de2621e9ea29d26
James <jameslopez@email.com>
2026-03-18 01:36:57 +0800

    feat: add responsive styles for branded typography

---
+++ styles/style.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+import { rMS } from "./responsive";
+
+export const Styles = StyleSheet.create({
+  budgEgoFont: {
+    fontSize: rMS(60, 0.7),
+  },
+});
