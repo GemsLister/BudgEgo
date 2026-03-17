f914035aac69d3ba349660984e0987e97b04a337
James <jameslopez@email.com>
2026-03-18 01:35:45 +0800

    feat: implement custom font loading in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import "@/global.css";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    "Google-Sans-Flex-Regular": require("@/assets/fonts/static/GoogleSansFlex_120pt-Regular.ttf"),
+  });
+
+  if (!fontsLoaded && !fontError) return null;
   return <Stack screenOptions={{ headerShown: false }} />;
 }
