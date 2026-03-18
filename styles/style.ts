c10b41d5c7e586300ea708bcce62a21ea1b34d1a
James <jameslopez@email.com>
2026-03-19 01:32:37 +0800

    feat: add responsive styles for login inputs and rename logo font constant

--- styles/style.ts
+++ styles/style.ts
@@ -1,8 +1,21 @@
 import { StyleSheet } from "react-native";
-import { rMS } from "./responsive";
+import { rMS, rS } from "./responsive";
 
 export const Styles = StyleSheet.create({
-  budgEgoFont: {
+  budgEgoFontLogo: {
     fontSize: rMS(60, 0.7),
+  },
+
+  inputSize: {
+    paddingHorizontal: rS(20),
+  },
+
+  inputTextSize: {
+    fontSize: rMS(16, 0.5),
+  },
+
+  inputFontSize: {
+    padding: rS(10),
+    fontSize: rMS(18, 0.8),
   },
 });
