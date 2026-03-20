610136b318597ad3d4357fa9e4c57dc895d71f87
James <jameslopez@email.com>
2026-03-21 00:31:39 +0800

    feat: add PrimaryButton for routing

--- screens/auth/LoginScreen.tsx
+++ screens/auth/LoginScreen.tsx
@@ -1,7 +1,8 @@
+import PrimaryButton from "@/components/ui/buttons/PrimaryButton";
 import { Styles } from "@/styles/style";
+import { Link } from "expo-router";
 import { Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
 export default function LoginScreen() {
   return (
     <SafeAreaView className="flex-1 justify-center gap-5 items-center bg-timberwolf">
@@ -20,12 +21,13 @@ export default function LoginScreen() {
         </Text>
       </View>
 
+      <View></View>
       <View className="gap-5 w-full">
         <View className="gap-2" style={Styles.inputSize}>
           <View>
             <Text
               style={Styles.inputTextSize}
-              className="font-google-sans-regular"
+              className="font-google-sans-regular font-[600] text-slate-600"
             >
               Email
             </Text>
@@ -38,7 +40,7 @@ export default function LoginScreen() {
         <View className="gap-2" style={Styles.inputSize}>
           <View className="mb-2">
             <Text
-              className="font-google-sans-regular"
+              className="font-google-sans-regular font-[600] text-slate-600"
               style={Styles.inputTextSize}
             >
               Password
@@ -46,10 +48,21 @@ export default function LoginScreen() {
           </View>
           <TextInput
             className="border border-slate-500 rounded-[10px]"
+            secureTextEntry
             style={Styles.inputFontSize}
           />
         </View>
+        <View className="items-center">
+          <Link href={"/"}>
+            <Text className="font-google-sans-regular text-slate-600">
+              Forgot Password?
+            </Text>
+          </Link>
+        </View>
       </View>
+      <Link href={"/"} asChild>
+        <PrimaryButton text={"Login"} />
+      </Link>
     </SafeAreaView>
   );
 }
